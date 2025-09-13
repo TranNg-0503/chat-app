6cb6f7ea46fec0915f70ad3a42d290c7a04380b2
MphuongL <phuong2442004@gmail.com>
2025-09-13 08:45:31 +0700

    p

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT =process.env.PORT;
 app.use(express.json());
-app.use("/api/auth",authRoutes);
+app.use("",authRoutes);
 app.listen(PORT,()=>
 {
     console.log('server is running on ${PORT}');
