ceae353313fcb81eb0fb5a630559ca784b1bb08a
hoanganh1401 <vohoanganh424@gmail.com>
2025-09-13 12:12:11 +0700

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,8 @@ import cors from "cors";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
-import userRoutes from "./routes/user.route.js";
-import chatRoutes from "./routes/chat.route.js";
+// import userRoutes from "./routes/user.route.js";
+// import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -26,8 +26,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("", authRoutes);
-app.use("/users", userRoutes);
-app.use("/chat", chatRoutes);
+// app.use("/users", userRoutes);
+// app.use("/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
