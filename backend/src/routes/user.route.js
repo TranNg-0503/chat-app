437ff73850513bb2b61c98bac64a3e55534e4699
MphuongL <phuong2442004@gmail.com>
2025-09-15 12:37:18 +0700

    phuong fr

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMyFriends, getRecommendedUsers, searchUsers } from "../controllers/user.controller.js"
+import { getMyFriends, getRecommendedUsers, searchUsers, sendFriendRequest, acceptFriendRequest,getOutgoingFriendReqs, getFriendRequests} from "../controllers/user.controller.js"
 const route = express.Router();
 
 route.use(protectRoute);
@@ -9,4 +9,10 @@ route.get("/", getRecommendedUsers);
 route.get("/friends", getMyFriends);
 route.get("/search", searchUsers);
 
+route.post("/friend-request/:id", sendFriendRequest);
+route.put("/friend-request/:id/accept", acceptFriendRequest);
+
+
+route.get("/friend-request", getFriendRequests);
+route.get("/outgoing-friend-request", getOutgoingFriendReqs);
 export default route;
