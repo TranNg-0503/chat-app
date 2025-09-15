750653d33b43496c25b84e423a7671c2cff8f6ec
MphuongL <phuong2442004@gmail.com>
2025-09-15 10:09:58 +0700

    phuong fr

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -31,3 +31,27 @@ export async function getMyFriends(req, res){
         res.status(500).json({message: "Lỗi server"});
     }
 }
+
+export async function searchUsers(req, res) {
+    try {
+        const { query } = req.query; // Lấy input từ query param, vd: /search?query=...
+        let users = [];
+
+        if (!query) {
+            return res.status(400).json({ message: "Vui lòng nhập query" });
+        }
+
+        if (query.includes("@gmail")) {
+            // Tìm chính xác email
+            users = await User.find({ email: query });
+        } else {
+            // Tìm gần đúng fullName, case-insensitive
+            users = await User.find({ fullName: { $regex: query, $options: "i" } });
+        }
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Lỗi trong searchUsers controller:", error.message);
+        res.status(500).json({ message: "Lỗi server" });
+    }
+}
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMyFriends, getRecommendedUsers } from "../controllers/user.controller.js"
+import { getMyFriends, getRecommendedUsers, searchUsers } from "../controllers/user.controller.js"
 const route = express.Router();
 
 route.use(protectRoute);
 
 route.get("/", getRecommendedUsers);
 route.get("/friends", getMyFriends);
+route.get("/search", searchUsers);
 
 export default route;
