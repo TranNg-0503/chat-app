c01204cc26c0310775fed4944cf60a15b5947622
TranNg-0503 <nguyenphamnhattran@gmail.com>
2025-09-23 13:28:47 +0700

    remove AuthProvider frm App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,55 +7,52 @@ import Friend from "./pages/friend";
 import UserChatPage from "./pages/user-chat";
 import ThemeProvider from "./components/providers/ThemeProvider";
 import ProtectedLayout from "./components/layouts/ProtecedLayout";
-import AuthProvider from "./components/providers/AuthProvider";
 
 export default function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <ProtectedLayout>
-                  <Dashboard />
-                </ProtectedLayout>
-              }
-            />
-            <Route path="/dashboard" element={<Navigate to="/" replace />} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedLayout>
+                <Dashboard />
+              </ProtectedLayout>
+            }
+          />
+          <Route path="/dashboard" element={<Navigate to="/" replace />} />
 
-            <Route
-              path="/onboard"
-              element={
-                <ProtectedLayout>
-                  <Onboard />
-                </ProtectedLayout>
-              }
-            />
-            <Route
-              path="/friends"
-              element={
-                <ProtectedLayout>
-                  <Friend />
-                </ProtectedLayout>
-              }
-            />
+          <Route
+            path="/onboard"
+            element={
+              <ProtectedLayout>
+                <Onboard />
+              </ProtectedLayout>
+            }
+          />
+          <Route
+            path="/friends"
+            element={
+              <ProtectedLayout>
+                <Friend />
+              </ProtectedLayout>
+            }
+          />
 
-            <Route
-              path="/chat-user"
-              element={
-                <ProtectedLayout>
-                  <UserChatPage />
-                </ProtectedLayout>
-              }
-            />
+          <Route
+            path="/chat-user"
+            element={
+              <ProtectedLayout>
+                <UserChatPage />
+              </ProtectedLayout>
+            }
+          />
 
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </AuthProvider>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
